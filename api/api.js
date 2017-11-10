018da152e7ad2f3dc9971368f6ad7f4f8c553b17
dromestrome <ashutoshc8101@gmail.com>
2017-11-10 18:15:12 +0530

    me route fixes

--- api/api.js
+++ api/api.js
@@ -70,16 +70,16 @@ Router.post('/auth/register',registerValidation.rules, registerValidation.nextMi
 
 Router.get('/auth/me',meValidation.rules, meValidation.nextMiddlware, function(req, res) {
   var token = req.headers['x-access-token'];
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  if (!token) return res.status(401).send({ status: false, message: 'No token provided.' });
   
   jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    if (err) return res.status(500).send({ status: false, message: 'Failed to authenticate token.' });
     
     User.findById(decoded.id, function (err, user) {
-      if (err) return res.status(500).send({msg: "There was a problem finding the user."});
-      if (!user) return res.status(404).send({msg: "No user found."});
+      if (err) return res.status(500).send({status: false, msg: "There was a problem finding the user."});
+      if (!user) return res.status(404).send({status: false, msg: "No user found."});
       
-      res.status(200).send(user);
+      res.status(200).send({ status: true, user: user});
     });
   });
 });
